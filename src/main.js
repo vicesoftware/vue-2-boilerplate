1d3b8df2656a5dd89bf77e0b3ceffcda326fe611
Ryan Vice <ryan.vice@plangrid.com>
2019-09-13 10:25:42 -0500

    fixed build issue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import Vue from 'vue'
-import upperFirst from 'lodash/upperFirst'
-import camelCase from 'lodash/camelCase'
 import App from './App.vue'
 import router from './router'
 import store from './store/store'
@@ -9,22 +7,6 @@ import BaseIcon from '@/modules/ui/BaseIcon'
 Vue.component('BaseIcon', BaseIcon)
 
 Vue.config.productionTip = false
-
-const requireComponent = require.context(
-  './components',
-  false,
-  /Base[A-Z]\w+\.(vue|js)$/
-)
-
-requireComponent.keys().forEach(fileName => {
-  const componentConfig = requireComponent(fileName)
-
-  const componentName = upperFirst(
-    camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1'))
-  )
-
-  Vue.component(componentName, componentConfig.default || componentConfig)
-})
 
 new Vue({
   router,
---
+++ src/views/ProgramSelection.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Program Selection</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
